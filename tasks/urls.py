731653010562866b50f318a8183559932a9a1ddd
Camila Marques <milamazim@gmail.com>
2026-03-22 21:24:16 -0300

    Some more adjustments in names and ordering

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from tasks import views
 
 urlpatterns = [
-    path('tasks/', views.list_tasks, name='list_tasks'),
-    path('new_task/', views.new_task, name='new_task'),
+    path('tasks/', views.task_list, name='task_list'),
+    path('tasks/new/', views.new_task, name='new_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render, redirect
 from tasks.models import Task
 from tasks.forms import TaskForm
 
-def list_tasks(request):
-    lists = Task.objects.all()
-    return render(request, 'list_task.html', {'tasks': lists})
+def task_list(request):
+    tasks = Task.objects.order_by('-created_on')
+    return render(request, 'list_task.html', {'tasks': tasks})
 
 def new_task(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('lists')
+            return redirect('task_list')
     else:
         form = TaskForm()
     
