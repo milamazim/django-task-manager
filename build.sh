79ba0b5fe3bf8d934e93806cbab3b0ecbcce3f14
Camila Marques <milamazim@gmail.com>
2026-03-26 17:17:53 -0300

    deploy: creating admin super user

--- build.sh
+++ build.sh
@@ -4,3 +4,18 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell << EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+EOF
