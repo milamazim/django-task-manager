f9ccd75002b572c4f182340f7e6aca90597e2e0c
Camila Marques <milamazim@gmail.com>
2026-03-26 16:45:32 -0300

    deploy: some adjustments in commands

--- build_files.sh
+++ build_files.sh
@@ -3,3 +3,15 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 
+python manage.py shell << EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+EOF
